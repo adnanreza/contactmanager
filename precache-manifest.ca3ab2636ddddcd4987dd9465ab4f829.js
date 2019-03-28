a30eacee8c8d0a7919aab7db7c04b89e7bc807d4
Adnan <adnan.reza@outlook.com>
2019-03-28 16:36:19 -0700

    Updates

---
+++ precache-manifest.ca3ab2636ddddcd4987dd9465ab4f829.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "c3b91de7cf438ed0f6ce",
+    "url": "/contactmanager/static/js/main.9f1cffcd.chunk.js"
+  },
+  {
+    "revision": "71e14ff2c5ae59a0b2d9",
+    "url": "/contactmanager/static/js/2.c81bbec4.chunk.js"
+  },
+  {
+    "revision": "c3b91de7cf438ed0f6ce",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "71e14ff2c5ae59a0b2d9",
+    "url": "/contactmanager/static/css/2.93c4108b.chunk.css"
+  },
+  {
+    "revision": "10e15a42112d44300fd38d2722dcdcc9",
+    "url": "/contactmanager/index.html"
+  }
+];
